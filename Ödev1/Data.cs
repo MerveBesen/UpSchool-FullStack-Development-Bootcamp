a7a52e7bfb8ce743aa4c5959904ebe73c2bff14f
MerveBesen <61238263+MerveBesen@users.noreply.github.com>
2023-02-28 20:38:36 +0300

    Data file added.

---
+++ Ödev1/Data.cs
@@ -0,0 +1,9 @@
+namespace PasswordGenerator.Console;
+
+public class Data
+{
+    public static string Numbers = "0123456789";
+    public static string LowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+    public static string UppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static string SpecialCharacters = "!'^+%&/()=?_*-";
+} 
