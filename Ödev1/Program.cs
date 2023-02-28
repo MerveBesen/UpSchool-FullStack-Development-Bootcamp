e876579b18340154eb61e14e559b59fdcba168a8
MerveBesen <61238263+MerveBesen@users.noreply.github.com>
2023-02-28 20:39:46 +0300

    PasswordGenerator added.

---
+++ Ödev1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PasswordGenerator.Console;
+
+Questions questions = new Questions();
+
+Console.WriteLine($"{questions.IncludeNumbers}");
+string Characters = string.Empty;
+
+string answer = Console.ReadLine();
+
+switch (answer)
+{
+    case "y":
+        
+      
+    case "n" :
+        Console.WriteLine("Ok. You wish :)");
+        break;
+    
+    
+    default:
+    // return Question
+        throw new Exception("You need to write y/n");
+    
+}
+
+Console.WriteLine(answer);
---
+++ Ödev1/Questions.cs
@@ -0,0 +1,10 @@
+namespace PasswordGenerator.Console;
+
+public class Questions
+{
+    public string IncludeNumbers => "Do you want to include numbers? y/n";
+    public string IncludeLowercaseCharacters => "Do you want to include lowercase characters? y/n";
+    public string IncludeUppercaseCharacters => "Do you want to include uppercase characters? y/n";
+    public string IncludeSpecialCharacters => "Do you want to include special characters? y/n";
+
+}
